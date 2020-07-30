2e0f3e6d9b52bda2e31527d25d1161f4109197cf
Adilson Tavares <adilson.tavares@nextem.com.br>
2020-07-30 17:43:41 -0400

    first test with jest

---
+++ src/filterByTerm.js
@@ -0,0 +1,10 @@
+function filterByTerm(inputArr, searchTerm) {
+
+    const regex = new RegExp(searchTerm, "i");
+    
+    return inputArr.filter(function(arrayElement) {
+        return arrayElement.url.match(regex);
+    });
+}
+
+module.exports = filterByTerm;
